6168153136e6519db23a88da06678e253de16977
chenwulou <wo6x5a@gmail.com>
2016-08-30 15:40:34 +0800

    bug fix

--- herakles/herakles-system/src/main/java/com/lcw/herakles/platform/system/files/consts/FileTemplateConsts.java
+++ herakles/herakles-system/src/main/java/com/lcw/herakles/platform/system/files/consts/FileTemplateConsts.java
@@ -10,9 +10,9 @@ public final class FileTemplateConsts {
 
     public static final String REDIRECT = "redirect:/web/system/file/template/download";
 
-    public static final String EXCEL_PATH = "excel/temp";
-    public static final String WORD_PATH = "word/temp";
-    public static final String PDF_PATH = "pdf/temp";
+    public static final String EXCEL_PATH = "excel/temp/";
+    public static final String WORD_PATH = "word/temp/";
+    public static final String PDF_PATH = "pdf/temp/";
 
     public final static String TEST_TEMP = "test_temp";
     public final static String TEST_TEMP_NAME = "测试模版";
