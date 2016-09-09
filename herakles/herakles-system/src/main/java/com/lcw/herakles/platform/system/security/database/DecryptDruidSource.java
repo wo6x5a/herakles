3ab0688f17861b19906b62994967dc83b7d67b90
chenwulou <wo6x5a@gmail.com>
2016-09-09 17:16:37 +0800

    encrypt druid

--- herakles/herakles-system/src/main/java/com/lcw/herakles/platform/system/security/database/DecryptDruidSource.java
+++ herakles/herakles-system/src/main/java/com/lcw/herakles/platform/system/security/database/DecryptDruidSource.java
@@ -1,30 +1,55 @@
 package com.lcw.herakles.platform.system.security.database;
 
-//import com.alibaba.druid.filter.config.ConfigTools;
-//import com.alibaba.druid.pool.DruidDataSource;
-//
-///**
-// * @author chenwulou
-// *
-// */
-//@SuppressWarnings("all")
-//public class DecryptDruidSource extends DruidDataSource {
-//	@Override
-//	public void setUsername(String username) {
-//		try {
-//			username = ConfigTools.decrypt(username);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		super.setUsername(username);
-//	}
-//	@Override
-//	public void setUrl(String url) {
-//		try {
-//			url = ConfigTools.decrypt(url);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		super.setUrl(url+"?useUnicode=true&characterEncoding=utf8&mysqlEncoding=utf8&useSSL=false");
-//	}
-//}
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.druid.filter.config.ConfigTools;
+import com.alibaba.druid.pool.DruidDataSource;
+
+/**
+ * @author chenwulou
+ *
+ */
+@SuppressWarnings("all")
+public class DecryptDruidSource extends DruidDataSource {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DecryptDruidSource.class);
+
+    @Override
+    public void setUsername(String username) {
+        try {
+            username = ConfigTools.decrypt(username);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+        super.setUsername(username);
+    }
+
+    @Override
+    public void setPassword(String password) {
+        try {
+            password = ConfigTools.decrypt(password);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+        super.setPassword(password);
+    }
+
+    @Override
+    public void setUrl(String url) {
+        try {
+            url = ConfigTools.decrypt(url);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+        super.setUrl(url + "?useUnicode=true&characterEncoding=utf-8&useSSL=false");
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(ConfigTools.encrypt("123456"));
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+}
