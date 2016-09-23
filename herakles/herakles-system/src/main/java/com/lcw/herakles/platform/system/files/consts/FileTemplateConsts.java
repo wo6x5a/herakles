7fe54c0640743c5e3c0247a58079dbd922d27273
chenwulou <wo6x5a@gmail.com>
2016-09-23 11:36:18 +0800

    update

--- herakles/herakles-system/src/main/java/com/lcw/herakles/platform/system/files/consts/FileTemplateConsts.java
+++ herakles/herakles-system/src/main/java/com/lcw/herakles/platform/system/files/consts/FileTemplateConsts.java
@@ -10,6 +10,7 @@ public final class FileTemplateConsts {
 
     public static final String REDIRECT = "redirect:/web/system/file/template/download";
 
+    /** 模板文件目录(src/main/resources/**), 文件分隔符(/)已经默认添加 **/
     public static final String EXCEL_PATH = "excel/temp/";
     public static final String WORD_PATH = "word/temp/";
     public static final String PDF_PATH = "pdf/temp/";
