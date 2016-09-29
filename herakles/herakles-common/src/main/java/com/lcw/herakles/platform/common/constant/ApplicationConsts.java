e7c7089c7ab474f3634d0e578685e16856f7e78b
chenwulou <wo6x5a@gmail.com>
2016-09-30 01:01:32 +0800

    update

--- herakles/herakles-common/src/main/java/com/lcw/herakles/platform/common/constant/ApplicationConsts.java
+++ herakles/herakles-common/src/main/java/com/lcw/herakles/platform/common/constant/ApplicationConsts.java
@@ -8,48 +8,49 @@ package com.lcw.herakles.platform.common.constant;
  */
 public final class ApplicationConsts {
 
-	private ApplicationConsts() {
+    private ApplicationConsts() {
 
-	}
+    }
 
-	public static final String GLOBAL_CONTEXT = "/web/";
+    public static final String GLOBAL_CONTEXT = "/web/";
 
-	public static final String DEFAULT_CREATE_OPID = "system";
+    public static final String DEFAULT_CREATE_OPID = "system";
 
-	public static final String X_FORM_ID = "x-form-id";
+    public static final String X_FORM_ID = "x-form-id";
 
-	public static final String MANUAL_VALIDATE = "manualValidate";
+    public static final String MANUAL_VALIDATE = "manualValidate";
 
-	/** Log discriminator **/
-	public static final String LOG_USER_NAME = "userName";
-	public static final String LOG_SESSION_ID = "sessionId";
+    /** Log discriminator **/
+    public static final String LOG_USER_NAME = "userName";
+    public static final String LOG_SESSION_ID = "sessionId";
 
-	/**
-	 * 默认最大支持导出数据条数
-	 */
-	public static final int EXPORT_DATA_MAX_SIZE = 50000;
+    /**
+     * 默认最大支持导出数据条数
+     */
+    public static final int EXPORT_DATA_MAX_SIZE = 50000;
 
-	/**
-	 * encoding
-	 */
-	public static final String UTF_8 = "UTF-8";
-	public static final String GB2312 = "GB2312";
+    /**
+     * encoding
+     */
+    public static final String UTF_8 = "UTF-8";
+    public static final String GB2312 = "GB2312";
 
-	/**
-	 * expend excel report
-	 */
-	public static final String REPORT_DATA = "data";
-	public static final String REPORT_FILE_NAME = "fileName";
-	public static final String REPORT_TEMP_PATH = "tempPath";
+    /**
+     * expend excel report
+     */
+    public static final String REPORT_DATA = "data";
+    public static final String REPORT_FILE_NAME = "fileName";
+    public static final String REPORT_TEMP_PATH = "tempPath";
 
-	/**
-	 * MD5默认迭代次数
-	 */
-	public static final int ITERATIONS = 3;
+    /**
+     * MD5默认迭代次数
+     */
+    public static final int ITERATIONS = 3;
+
     /**
      * 水印图片的路径
      */
-//    public static final String WATERMARK_PATTERN = "image/watermark{0}.png";
-	public static final String WATERMARK_PATTERN_PATH = "image/watermark_logo.png";
+    // public static final String WATERMARK_PATTERN = "image/watermark{0}.png";
+    public static final String WATERMARK_PATTERN_PATH = "image/watermark_logo.png";
 
 }
