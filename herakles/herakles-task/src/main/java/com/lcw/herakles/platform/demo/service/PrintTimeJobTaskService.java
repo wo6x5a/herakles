38d59705fc188a653f8d9a69e63bee801b0779c9
chenwulou <wo6x5a@gmail.com>
2016-09-30 11:54:50 +0800

    split task from the project

---
+++ herakles/herakles-task/src/main/java/com/lcw/herakles/platform/demo/service/PrintTimeJobTaskService.java
@@ -0,0 +1,21 @@
+package com.lcw.herakles.platform.demo.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrintTimeJobTaskService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrintTimeJobTaskService.class);
+
+    // @Autowired
+    // private ProductRepository productRepository;
+
+    public void getTime() {
+        LOGGER.debug("this is task demo");
+        // System.out.println(productRepository.findAll().size());
+//        System.out.println(new Date());
+    }
+
+}
