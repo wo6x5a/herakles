85eda379ef3c85b96c3c48f1659fd4ccd9acfd91
chenwulou <wo6x5a@gmail.com>
2016-10-02 20:10:01 +0800

    update

--- herakles/herakles-task/src/main/java/com/lcw/herakles/platform/demo/service/PrintTimeJobTaskService.java
+++ herakles/herakles-task/src/main/java/com/lcw/herakles/platform/demo/service/PrintTimeJobTaskService.java
@@ -4,18 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author chenwulou
+ *
+ */
 @Service
 public class PrintTimeJobTaskService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PrintTimeJobTaskService.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(PrintTimeJobTaskService.class);
 
-    // @Autowired
-    // private ProductRepository productRepository;
+	// @Autowired
+	// private ProductRepository productRepository;
 
-    public void getTime() {
-        LOGGER.debug("this is task demo");
-        // System.out.println(productRepository.findAll().size());
-//        System.out.println(new Date());
-    }
+	public void getTime() {
+		LOGGER.debug("this is task demo");
+		// System.out.println(productRepository.findAll().size());
+		// System.out.println(new Date());
+	}
 
 }
