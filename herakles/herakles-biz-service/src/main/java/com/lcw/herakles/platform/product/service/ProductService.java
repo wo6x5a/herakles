9079042d959eacde3443606209129cb9dab7cca5
chenwulou <wo6x5a@gmail.com>
2016-12-22 16:33:16 +0800

    业务日志 用例 add

--- herakles/herakles-biz-service/src/main/java/com/lcw/herakles/platform/product/service/ProductService.java
+++ herakles/herakles-biz-service/src/main/java/com/lcw/herakles/platform/product/service/ProductService.java
@@ -10,6 +10,8 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.lcw.herakles.platform.bizlog.enums.EOptType;
+import com.lcw.herakles.platform.bizlog.service.BizLogService;
 import com.lcw.herakles.platform.common.converter.ConverterService;
 import com.lcw.herakles.platform.common.enums.EErrorCode;
 import com.lcw.herakles.platform.common.service.BaseService;
@@ -17,6 +19,7 @@ import com.lcw.herakles.platform.common.util.ErrorUtil;
 import com.lcw.herakles.platform.product.dto.req.ProductReqDto;
 import com.lcw.herakles.platform.product.entity.ProductPo;
 import com.lcw.herakles.platform.product.repository.ProductRepository;
+import com.lcw.herakles.platform.system.security.SecurityContext;
 
 /**
  * Class Name: ProductService Description: TODO
@@ -29,6 +32,8 @@ public class ProductService extends BaseService{
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private BizLogService bizLogService;
 
     @Cacheable(value = "PRODUCT_CACHE", key = "'PRODUCT_' + #id")
     @Transactional(readOnly = true)
@@ -51,11 +56,22 @@ public class ProductService extends BaseService{
     @Transactional
     public ProductPo saveProduct(ProductReqDto productReqDto) {
         ProductPo product = new ProductPo();
-        if (StringUtils.isNotBlank(productReqDto.getId())) {
+        boolean isEdit = StringUtils.isNotBlank(productReqDto.getId());
+        ProductPo oldPo = new ProductPo();
+        if (isEdit) {
             product = productRepository.findOne(productReqDto.getId());
+            oldPo = ConverterService.convert(product, oldPo);
             super.setUpdateInfo(product);
         }
         product = ConverterService.convert(productReqDto, product);
+
+        // add bizlog
+        if (isEdit) {
+            bizLogService.saveEditLog(ProductPo.class, EOptType.UPDATE, "修改product",
+                    productReqDto.getId(), SecurityContext.getInstance().getCurrentOperatorId(),
+                    oldPo, product, "");
+        }
+
         super.setCreateInfo(product);
         return productRepository.save(product);
 
