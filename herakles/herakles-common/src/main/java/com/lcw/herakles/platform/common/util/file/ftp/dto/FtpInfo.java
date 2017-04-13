29c402280c889c364442d41630c3c8bfadb8d8f4
wo6x5a <wo6x5a@gmail.com>
2017-04-13 13:34:48 +0800

    update

--- herakles/herakles-common/src/main/java/com/lcw/herakles/platform/common/util/file/ftp/dto/FtpInfo.java
+++ herakles/herakles-common/src/main/java/com/lcw/herakles/platform/common/util/file/ftp/dto/FtpInfo.java
@@ -12,47 +12,52 @@ public class FtpInfo {
 
     private static final String FTP_AES_KEY = "5QSC8vwvBi7ohPkukgT0Bw==";
 
-	private String ftpHost;
+    private String ftpHost;
 
-	private String ftpUserName;
+    private String ftpUserName;
 
-	private String ftpPassword;
+    private String ftpPassword;
 
-	private int ftpPort;
+    private int ftpPort;
 
-	public String getFtpHost() {
-		return ftpHost;
-	}
+    public String getFtpHost() {
+        return ftpHost;
+    }
 
-	public void setFtpHost(String ftpHost) {
-	    ftpHost = EndecryptUtil.decryptAes(ftpHost, FTP_AES_KEY);
-		this.ftpHost = ftpHost;
-	}
+    public void setFtpHost(String ftpHost) {
+        ftpHost = EndecryptUtil.decryptAes(ftpHost, FTP_AES_KEY);
+        this.ftpHost = ftpHost;
+    }
 
-	public String getFtpUserName() {
-		return ftpUserName;
-	}
+    public String getFtpUserName() {
+        return ftpUserName;
+    }
 
-	public void setFtpUserName(String ftpUserName) {
-	    ftpUserName = EndecryptUtil.decryptAes(ftpUserName, FTP_AES_KEY);
-		this.ftpUserName = ftpUserName;
-	}
+    public void setFtpUserName(String ftpUserName) {
+        ftpUserName = EndecryptUtil.decryptAes(ftpUserName, FTP_AES_KEY);
+        this.ftpUserName = ftpUserName;
+    }
 
-	public String getFtpPassword() {
-		return ftpPassword;
-	}
+    public String getFtpPassword() {
+        return ftpPassword;
+    }
 
-	public void setFtpPassword(String ftpPassword) {
-	    ftpPassword = EndecryptUtil.decryptAes(ftpPassword, FTP_AES_KEY);
-		this.ftpPassword = ftpPassword;
-	}
+    public void setFtpPassword(String ftpPassword) {
+        ftpPassword = EndecryptUtil.decryptAes(ftpPassword, FTP_AES_KEY);
+        this.ftpPassword = ftpPassword;
+    }
 
-	public int getFtpPort() {
-		return ftpPort;
-	}
+    public int getFtpPort() {
+        return ftpPort;
+    }
 
-	public void setFtpPort(int ftpPort) {
-		this.ftpPort = ftpPort;
-	}
+    public void setFtpPort(int ftpPort) {
+        this.ftpPort = ftpPort;
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println(EndecryptUtil.encryptAes(
+//                "172.16.31.119", FTP_AES_KEY));
+//    }
 
 }
